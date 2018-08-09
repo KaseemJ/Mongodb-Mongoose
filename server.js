21d56b8eef34ef451043e74c70462c7d02d9f62c
wesley9mm <info.developer4@gmail.com>
2018-08-08 23:49:47 -0700

    add

--- server.js
+++ server.js
@@ -17,50 +17,50 @@ var app = express();
 app.use(express.static("public"));
 
 // Connect Handlebars to our Express app
- app.engine("handlebars", exphbs({ defaultLayout: "main" }));
- app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Use bodyParser in our app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// If deployed, use the deployed database. Otherwise use the local movie database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/movie";
+// If deployed, use the deployed database. Otherwise use the local news database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news";
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
 
 // GET all the articles that favorite is set to false
 // and render them to the index.handlebars page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   db.#.find({ favorite: false })
-    .then(function(data){
+    .then(function (data) {
       // res.json(data);
-      res.render("index", {#: data});
-    }).catch(function(err){
+      res.render("index", { #: data });
+    }).catch(function (err) {
       res.status(404).send(err);
     });
 });
 
 // GET all the articles that favorite is set to true
 // and render them to the favorite.handlebars page
-app.get("/#", function(req, res) {
-  db.#.find({ favorite: true})
-    .then(function(data){
+app.get("/#", function (req, res) {
+  db.#.find({ favorite: true })
+    .then(function (data) {
       // res.json(data);
-      res.render("#", {movie: data});
-    }).catch(function(err){
+      res.render("#", { movie: data });
+    }).catch(function (err) {
       res.status(404).send(err);
     });
 });
 
 // POST an Article to the mongo database
-app.post("/api/#", function(req, res){
+app.post("/api/#", function (req, res) {
   db.#.create(req.body)
-    .then(function(){
+    .then(function () {
       // res.json(db#);
       res.redirect("/")
-    }).catch(function(err){
+    }).catch(function (err) {
       res.status(400).send(err);
     });
 });
@@ -68,16 +68,16 @@ app.post("/api/#", function(req, res){
 // PUT (UPDATE) article by its _id 
 // Will set the movie favorite to whatever the value 
 // of the req.body.favorite boolean is
-app.put("/api/article/:id", function(req, res){
-  db.Movie.findByIdAndUpdate(req.params.id, {favorite: req.body.favorite}, {new: true})
-    .then(function(dbArticle){
+app.put("/api/article/:id", function (req, res) {
+  db.Movie.findByIdAndUpdate(req.params.id, { favorite: req.body.favorite }, { new: true })
+    .then(function (dbArticle) {
       res.json(dbArticle);
-    }).catch(function(err){
+    }).catch(function (err) {
       res.status(400).send(err);
     });
 });
 
 // Listen on the port
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Listening on port: " + PORT);
 });
